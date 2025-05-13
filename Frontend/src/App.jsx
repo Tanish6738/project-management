34b80e2bbec39f72193b140543919b49fd580ba2
Tanish6738 <123645393+Tanish6738@users.noreply.github.com>
2025-05-13 16:40:33 +0530

    Add development roadmap and backend design documents for MERN Project Management App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,41 +1,9 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import AppRoutes from './Routes/AppRoutes'
-import { ProjectProvider } from './App/Context/ProjectContext'
-import { AuthProvider } from './App/Context/AuthContext'
-import { TaskProvider } from './App/Context/TaskContext'
-import { TeamProvider } from './App/Context/TeamContext'
-import { UserProvider } from './App/Context/UserContext'
-import { TimelogProvider } from './App/Context/TimelogContext'
-import { Toaster } from 'react-hot-toast'
-
+import Landing from "../src/LandingPage/Landing"
 const App = () => {
   return (
-    <BrowserRouter>
-      <UserProvider>
-        <AuthProvider>
-          <ProjectProvider>
-            <TaskProvider>
-              <TeamProvider>
-                <TimelogProvider>
-                  <AppRoutes />
-                  <Toaster 
-                    position="top-right"
-                    toastOptions={{
-                      duration: 5000,
-                      style: {
-                        background: '#18181b',
-                        color: '#fff',
-                      },
-                    }}
-                  />
-                </TimelogProvider>
-              </TeamProvider>
-            </TaskProvider>
-          </ProjectProvider>
-        </AuthProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <>
+    <Landing />
+    </>
   )
 }
 
