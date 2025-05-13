d9aad221f9d650f3c1fa030ddc6f3f3d9bf8a5ad
Tanish6738 <123645393+Tanish6738@users.noreply.github.com>
2025-05-13 16:46:38 +0530

    Video

--- Frontend/src/LandingPage/PreHero.jsx
+++ Frontend/src/LandingPage/PreHero.jsx
@@ -13,7 +13,7 @@ const PreHero = () => {
           className="w-full h-full object-cover opacity-90"
         >
           {/* <source src="/Videos/Untitled design.mp4" type="video/mp4" /> */}
-          <source src="/Videos/opt (4).mp4" type="video/mp4" />
+          <source src="/MP.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-black/60" />
